40d565b25fe437dfad52ac69ff97d2ea70f269e6
mengx520 <mengx520@gmail.com>
2021-07-30 00:13:42 -0700

    Add error handler

--- app.py
+++ app.py
@@ -222,6 +222,62 @@ def create_app():
         abort(422)
   
 
+  # Error Handling
 
+  @app.errorhandler(422)
+  def unprocessable(error):
+      return jsonify({
+          "success": False,
+          "error": 422,
+          "message": "unprocessable"
+      }), 422
+
+  '''
+  implement error handlers using the @app.errorhandler(error) decorator
+  '''
+
+  @app.errorhandler(400)
+  def bad_request(error):
+      return jsonify({
+          "success": False,
+          "error": 400,
+          "message": 'Bad Request'
+      }), 400
+
+
+  @app.errorhandler(401)
+  def unauthorized(error):
+      return jsonify({
+          "success": False,
+          "error": 401,
+          "message": 'Unathorized'
+      }), 401
+
+
+  @app.errorhandler(404)
+  def not_found(error):
+      return jsonify({
+          "success": False,
+          "error": 404,
+          "message": "resource not found"
+      }), 404
+
+
+  @app.errorhandler(500)
+  def internal_server_error(error):
+      return jsonify({
+          "success": False,
+          "error": 500,
+          "message": 'Internal Server Error'
+      }), 500
+
+
+  @app.errorhandler(AuthError)
+  def auth_error(error):
+      return jsonify({
+          "success": False,
+          "error": error.status_code,
+          "message": "authorization error"
+      }), error.status_code
 
   return app
