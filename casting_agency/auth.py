7efcb7fba0862f0976db3626221b6b0a717170dd
mengx520 <mengx520@gmail.com>
2021-08-02 10:31:31 -0700

    Update bash file

--- casting_agency/auth.py
+++ casting_agency/auth.py
@@ -6,10 +6,16 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
-
+'''
 AUTH0_DOMAIN = 'xiaohan.us.auth0.com'
 API_AUDIENCE = 'casting_agency'
-ALGORITHMS = ["RS256"]
+ALGORITHMS = ['RS256']
+'''
+
+
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 # AuthError Exception
 '''
